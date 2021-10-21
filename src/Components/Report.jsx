d0495bc6704bf1f6c2885a466bcdebd09828e21f
ianwhitehill <smwirbw@gmail.com>
2021-10-21 10:35:14 -0500

    update2.0

--- src/Components/Report.jsx
+++ src/Components/Report.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import sssImage from '../Img/sss.jpg';
+import sssImage from '../Img/all.jpg';
 
 const Report = (props) => {
     return (
@@ -7,9 +7,9 @@ const Report = (props) => {
             <h3>Length of side A of the triangle is {props.triangle.hypotenuse} millimetres</h3>
             <h3>Length of side B of the triangle is {props.triangle.adjacent} millimetres</h3>
             <h3>Length of side C of the triangle is {props.triangle.opposite} millimetres</h3>
-            <h3>Angle of corner A of the triangle in {props.triangle.angleA} degrees</h3>
-            <h3>Angle of corner B of the triangle in {props.triangle.angleB} degrees</h3>
-            <h3>Angle of corner C of the triangle in {props.triangle.angleC} degrees</h3>
+            <h3>Angle of corner <i>a</i> of the triangle in {props.triangle.angleA} degrees</h3>
+            <h3>Angle of corner <i>b</i> of the triangle in {props.triangle.angleB} degrees</h3>
+            <h3>Angle of corner <i>c</i> of the triangle in {props.triangle.angleC} degrees</h3>
             <h2>Triangle Classification: {props.triangle.classification}</h2>
             <h2>Traingle Categorization: {props.triangle.categorization}</h2>
             <div><a href="http://localhost:3000/selection" className="btn btn-outline-success buttonPad">Finished</a><img src={sssImage} /></div>
